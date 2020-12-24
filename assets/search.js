d38e85b4defe6ec27c1982aae4f2673653963c7c
arisac <arisac@tomsc.com>
2020-12-24 00:39:56 -0500

    fix #24

--- assets/search.js
+++ assets/search.js
@@ -14,7 +14,6 @@
   input.addEventListener('focus', init);
   input.addEventListener('focus', search);
   input.addEventListener('keyup', search);
-  input.addEventListener('change', search);
 
   document.addEventListener('keypress', focusSearchFieldOnKeyPress);
 
